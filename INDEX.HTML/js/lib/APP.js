a340f5aa5e19a623cbeb31724677076c30ec0b23
RenatoPolo <releo-46@hotmail.com>
2020-12-07 22:17:37 -0500

    ISUU
    HTML PURE

---
+++ INDEX.HTML/js/lib/APP.js
@@ -0,0 +1,38 @@
+(function () {
+
+    var app = angular.module('pokedex', [
+      'ngRoute',
+      'pokedex.controllers',
+      'pokedex.directives',
+      'pokedex.filters',
+      'pokedex.services'
+    ]);
+  
+    app.config(['$routeProvider', function ($routeProvider) {
+  
+      $routeProvider
+        .when('/', {
+          templateUrl: 'views/pokedex.html',
+          controller: 'PokedexController'
+        })
+        .when('/:type', {
+          templateUrl: 'views/pokedex.html',
+          controller: 'PokedexController'
+        })
+        .when('/pokemon/:name', {
+          templateUrl: 'views/pokemon.html',
+          controller: 'PokemonController'
+        })
+        .otherwise({
+          redirectTo: '/'
+        });
+    app.controller('CommentController',function(){
+        this.comments =[];
+        this.show = true;
+
+    }
+  
+    }]);
+  
+  })();
+    
